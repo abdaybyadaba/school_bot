c478cb8d300fdbbe42e124e172022b964745981f
abdaybyadaba <o.maxsv508@gmail.com>
2023-07-30 19:04:11 +0300

    new link to portal

--- parse.py
+++ parse.py
@@ -13,7 +13,7 @@ options.add_argument("--start-maximized")
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
-SCHOLL_PAGE = "https://schools.school.mosreg.ru/marks.aspx?school=52898&index=7&tab=period&homebasededucation=False"
+SCHOLL_PAGE = "https://login.school.mosreg.ru/?ReturnUrl=https%3a%2f%2fschools.school.mosreg.ru%2fschool.aspx"
 
 
 def open_browser(options):
