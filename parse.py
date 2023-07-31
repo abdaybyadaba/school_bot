88fad2d044e63ff162dae822958a65901304795c
abdaybyadaba <o.maxsv508@gmail.com>
2023-07-31 21:47:43 +0300

    4 attempt to fix problems

--- parse.py
+++ parse.py
@@ -11,7 +11,7 @@ from pyvirtualdisplay import Display
 auth_config = dotenv_values(".env")
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)  # удержание страницы открытой
-options.add_argument("--start-maximized")
+#options.add_argument("--start-maximized")
 options.add_argument('--disable-dev-shm-usage')
 # options.add_argument("--headless")
 options.add_argument("--no-sandbox")
