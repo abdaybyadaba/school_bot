e0d01d2aa40b3d341ddad2183da06e3b91a1688c
abdaybyadaba <o.maxsv508@gmail.com>
2023-08-02 08:37:18 +0300

    test 2.4

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)  # удержание страницы открытой
 #options.add_argument("--start-maximized")
 options.add_argument("--no-sandbox")
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 #options.add_argument('--disable-dev-shm-usage')
 
