23da9013f7ba9928225f3dd36cca6891031c60af
abdaybyadaba <o.maxsv508@gmail.com>
2023-08-02 12:05:40 +0300

    test 3

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)  # удержание страницы открытой
 options.add_argument("--start-maximized")
 options.add_argument("--no-sandbox")
-options.add_argument("--headless")
+#options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument('--disable-dev-shm-usage')
 
@@ -19,9 +19,14 @@ bw.get("https://login.school.mosreg.ru/login/?ReturnUrl=https%3A%2F%2Fschool.mos
 time.sleep(5)
 print("1", bw)
 print(bw.page_source, bw.current_url)
-bw.get("https://login.school.mosreg.ru/login/?ReturnUrl=https%3A%2F%2Fschool.mosreg.ru%2Fuserfeed")
-print("1", bw)
-print(bw.page_source, bw.current_url)
+i = 0
+while True:
+    if not i % 10:
+        print(bw.page_source, i)
+    if bw.page_source != "<html><head></head><body></body></html>":
+        break
+    i += 1
+
 log = bw.find_element(By.CLASS_NAME, "mosreg-login-block__content").find_element(By.NAME, "mosreg-login-form")
 print("2", bw, log)
 log.send_keys("spd")
